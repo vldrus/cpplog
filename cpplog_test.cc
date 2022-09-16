ddd1030227b3167b3ac6c4d5fd8367f9064292ab
Vlad <vldrus@gmail.com>
2022-09-16 08:20:45 +0300

    Some improvements

--- cpplog_test.cc
+++ cpplog_test.cc
@@ -16,8 +16,8 @@ int main()
     });
 
     LOG_DEBUG << "This is debug";
-    LOG_INFO << "This is info";
-    LOG_WARN << "This is warn";
+    LOG_INFO  << "This is info";
+    LOG_WARN  << "This is warn";
     LOG_ERROR << "This is error";
 
     th.join();
